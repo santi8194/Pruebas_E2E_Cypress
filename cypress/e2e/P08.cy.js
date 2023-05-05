c2d4c7c9c71ebb41b918df1d69eac815f08ce416
Laura Molina <ld.molina11@uniandes.edu.co>
2023-05-04 20:51:59 -0500

    Se crea el script para el escenario P08

---
+++ cypress/e2e/P08.cy.js
@@ -0,0 +1,66 @@
+import { faker } from "@faker-js/faker";
+
+import PageSection from "../support/elements/pagesSection";
+import AdminMenu from "../support/elements/adminMenu";
+import Site from "../support/elements/site";
+
+const pageSection = new PageSection();
+const adminMenu = new AdminMenu();
+const site = new Site();
+
+describe("Edición unicamente del título de una página existente y validación de cambios en la aplicación.", () => {
+  it("Edición unicamente del título de una página existente y validación de cambios en la aplicación.", () => {
+    /* 
+    -------------
+      GIVEN
+    -------------
+    */
+
+    // Autentica un usuario que puede crear páginas
+    cy.login();
+
+    // Va a la pestaña Pages
+    adminMenu.pageTab.click();
+    cy.wait(1000);
+
+    // Crea la página a editar
+    const title = faker.lorem.lines(1);
+    const content = faker.lorem.paragraphs(1);
+
+    pageSection.createPage(title, content);
+
+    // Publica la página
+    pageSection.publishPage();
+    pageSection.goBackToPagesSection.click();
+
+    /* 
+    -------------
+      WHEN
+    -------------
+    */
+
+    // Selecciona la página a editar
+    pageSection.pageInList(title).click();
+
+    // Edita el cuerpo de una página
+    const newContent = faker.lorem.paragraphs(1);
+    pageSection.editorContainerBody.clear().type(newContent);
+    pageSection.editorUpdateDropdown.click();
+    pageSection.editorUpdateButton.click();
+    cy.wait(3000);
+
+    /* 
+    -------------
+      THEN
+    -------------
+    */
+
+    // Verifica que la página aparezca visible en el sitio con el nuevo contenido
+    pageSection.editorSettingsButton.click();
+    pageSection.editorViewPage.invoke("attr", "href").then((href) => {
+      cy.visit(href);
+    });
+    cy.wait(1000);
+    site.pageContent.contains(newContent);
+  });
+});
