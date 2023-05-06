e34b8438ea4cac9615720640e56dc52ff4d89096
Camilo Ramírez R <c.ramirezr2@uniandes.edu.co>
2023-05-06 18:08:51 -0500

    Se completa P18.cy.js

--- cypress/e2e/P18.cy.js
+++ cypress/e2e/P18.cy.js
@@ -1,6 +1,10 @@
 import AdminMenu from "../support/elements/adminMenu";
+import DesignSection from "../support/elements/designSection";
 
 const adminMenu = new AdminMenu();
+const designSection = new DesignSection();
+const label = 'AnotherHome';
+const url = '/AnotherHome/';
 
 describe('Agregar link de navegación', () => {
     it('Agregar un nuevo link de navegación con Label AnotherHome', () => {
@@ -13,5 +17,25 @@ GIVEN
         cy.login();
         // Design Ir a la pestaña Design
         adminMenu.designTab.click();
+        cy.wait(1000);
+        /*
+-------------
+WHEN
+-------------
+*/
+        // Agregar Label AnotherHome con url http://localhost:2368/AnotherHome/
+        designSection.createLink(label, url);
+        cy.wait(1000);
+        // Guardar cambios
+        designSection.saveButton.click();
+        cy.wait(2000);
+        /*
+-------------
+THEN
+-------------
+*/
+        // Verificar existencia de nuevo url
+        designSection.verifyNewLabel;
+        designSection.verifyNewUrl;
     })
 })
