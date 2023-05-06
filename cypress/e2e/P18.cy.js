f0a2167b090359b3a4b8c40882fb701f0642ee7b
Camilo Ramírez R <c.ramirezr2@uniandes.edu.co>
2023-05-06 15:21:04 -0500

    Se crea script P18.cy.js

---
+++ cypress/e2e/P18.cy.js
@@ -0,0 +1,7 @@
+import AdminMenu from "../support/elements/adminMenu";
+
+const adminMenu = new AdminMenu();
+
+describe('Agregar link de navegación', () => {
+    it('Agregar un nuevo link de navegación con Label AnotherHome', () => {})
+})
