59fd69c1442a604edf042969869205ca97ff10ea
Laura Molina <ld.molina11@uniandes.edu.co>
2023-05-07 16:06:13 -0500

    Modificación escenario

--- cypress/support/elements/designSection.js
+++ cypress/support/elements/designSection.js
@@ -1,49 +1,51 @@
 export default class DesignSection {
-    get labelInput() {
-        return cy.get('input[placeholder="Label"]').eq(-2);
-    }
-    get addButton() {
-        return cy.get("button").contains("Add");
-    }
-    createLink(label) {
-        this.labelInput.type(label);
-        this.addButton.click({force: true});
-    }
-    get saveButton() {
-        return cy.get("button").contains("Save");
-    }
-    get verifyNewLabel() {
-        return cy.get('input[placeholder="Label"]')
-            .should(($inputs) => {
-                expect($inputs.toArray().some((input) => input.value === 'AnotherHome')).to.be.true;
-            });
-    }
-    get labelToChange() {
-        return cy.get('input[placeholder="Label"]').eq(2);
+  get labelInput() {
+    return cy.get('input[placeholder="Label"]').eq(-2);
+  }
+  get addButton() {
+    return cy.get("button").contains("Add");
+  }
+  createLink(label) {
+    this.labelInput.type(label);
+    this.addButton.click({ force: true });
+  }
+  get saveButton() {
+    return cy.get("button").contains("Save");
+  }
+  get verifyNewLabel() {
+    return cy.get('input[placeholder="Label"]').should(($inputs) => {
+      expect($inputs.toArray().some((input) => input.value === "AnotherHome"))
+        .to.be.true;
+    });
+  }
 
-    }
-    editLabel(addedName) {
-        this.labelToChange.type(addedName);
-    }
-    get editedLabel() {
-        return cy.get('input[placeholder="Label"][id="ember140"]');
-    }
-    get clickHelpDeleteButton() {
-       return cy
-           .get('button')
-           .filter(':contains("Delete")')
-           .last()
-           .then(($button) => {
-               cy.wrap($button).click();
-           })
-    }
-    get validateDeletedLink() {
-        return cy.get('input[placeholder="Label"]').each(($input) => {
-            cy.wrap($input)
-                .invoke('val')
-                .then((value) => {
-                    expect(value).to.not.equal('Help');
-                })
-        })
-    }
+  get navigationLabels() {
+    return cy.get("#settings-navigation input[placeholder='Label']");
+  }
+
+  editFirstLabel(value) {
+    this.navigationLabels.then((inputs) => {
+      cy.wait(500);
+      cy.wrap(inputs[0]).invoke("val", "").type(value);
+    });
+  }
+
+  get clickHelpDeleteButton() {
+    return cy
+      .get("button")
+      .filter(':contains("Delete")')
+      .last()
+      .then(($button) => {
+        cy.wrap($button).click();
+      });
+  }
+  get validateDeletedLink() {
+    return cy.get('input[placeholder="Label"]').each(($input) => {
+      cy.wrap($input)
+        .invoke("val")
+        .then((value) => {
+          expect(value).to.not.equal("Help");
+        });
+    });
+  }
 }
