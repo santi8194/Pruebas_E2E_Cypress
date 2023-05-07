56b45c87d78fb1a91c72dc69ecec65bac603597a
Laura Molina <ld.molina11@uniandes.edu.co>
2023-05-07 16:06:13 -0500

    Modificación escenario

--- cypress/e2e/P19.cy.js
+++ cypress/e2e/P19.cy.js
@@ -1,39 +1,62 @@
 import AdminMenu from "../support/elements/adminMenu";
 import DesignSection from "../support/elements/designSection";
+import { faker } from "@faker-js/faker";
 
 const adminMenu = new AdminMenu();
 const designSection = new DesignSection();
-const addedname = 'Editado';
-const editResult = 'Author' + addedname;
+const labelValue = faker.word.noun();
+const newValue = faker.word.noun();
 
-describe('Editar link de navegación', () => {
-    it('Edita el link con el label author', () => {
-        /*
--------------
-GIVEN
--------------
-*/
-        // Autenticar usuario
-        cy.login();
-        cy.wait(1000);
-        // Design Ir a la pestaña Design
-        adminMenu.designTab.click();
-        cy.wait(1000);
-        /*
--------------
-WHEN
--------------
-*/
-        // Editar Link the autor
-       designSection.editLabel(addedname);
-       designSection.saveButton.click();
-       cy.wait(1000);
-        /*
+describe("Editar únicamente el label de un link y verificar el cambio.", () => {
+  it("Editar únicamente el label de un link y verificar el cambio.", () => {
+    /*
+		-------------
+		GIVEN
+		-------------
+		*/
+
+    // Autenticar usuario
+    cy.login();
+    cy.wait(1000);
+
+    // Ir a la pestaña Design
+    adminMenu.designTab.click();
+    cy.wait(1000);
+
+    // Crear link a editar
+    designSection.createLink(labelValue);
+    cy.wait(1000);
+
+    // Guardar cambios
+    designSection.saveButton.click();
+    cy.wait(2000);
+
+    // Recargar settings
+    cy.reload();
+
+    /*
+		-------------
+		WHEN
+		-------------
+		*/
+
+    // Editar el label creado
+    designSection.editLastLabel(newValue);
+    designSection.saveButton.click();
+    cy.wait(1000);
+    /*
 -------------
 THEN
 -------------
 */
-        //Verificar los cambios
-        designSection.labelToChange.should('have.value', editResult);
-    })
-})
+    // Recargar settings
+    cy.reload();
+    // Verificar los cambios
+    designSection.navigationLabels.then((inputs) => {
+      cy.wait(500);
+      cy.wrap(inputs[inputs.length - 2])
+        .invoke("val")
+        .should("eq", newValue);
+    });
+  });
+});
--- cypress/support/elements/designSection.js
+++ cypress/support/elements/designSection.js
@@ -1,49 +1,53 @@
 export default class DesignSection {
-    get labelInput() {
-        return cy.get('input[placeholder="Label"]').eq(-2);
-    }
-    get addButton() {
-        return cy.get("button").contains("Add");
-    }
-    createLink(label) {
-        this.labelInput.type(label);
-        this.addButton.click({force: true});
-    }
-    get saveButton() {
-        return cy.get("button").contains("Save");
-    }
-    get verifyNewLabel() {
-        return cy.get('input[placeholder="Label"]')
-            .should(($inputs) => {
-                expect($inputs.toArray().some((input) => input.value === 'AnotherHome')).to.be.true;
-            });
-    }
-    get labelToChange() {
-        return cy.get('input[placeholder="Label"]').eq(2);
+  get labelInput() {
+    return cy.get('input[placeholder="Label"]').eq(-2);
+  }
+  get addButton() {
+    return cy.get("button").contains("Add");
+  }
+  createLink(label) {
+    this.labelInput.type(label);
+    this.addButton.click({ force: true });
+  }
+  get saveButton() {
+    return cy.get("button").contains("Save");
+  }
+  get verifyNewLabel() {
+    return cy.get('input[placeholder="Label"]').should(($inputs) => {
+      expect($inputs.toArray().some((input) => input.value === "AnotherHome"))
+        .to.be.true;
+    });
+  }
 
-    }
-    editLabel(addedName) {
-        this.labelToChange.type(addedName);
-    }
-    get editedLabel() {
-        return cy.get('input[placeholder="Label"][id="ember140"]');
-    }
-    get clickHelpDeleteButton() {
-       return cy
-           .get('button')
-           .filter(':contains("Delete")')
-           .last()
-           .then(($button) => {
-               cy.wrap($button).click();
-           })
-    }
-    get validateDeletedLink() {
-        return cy.get('input[placeholder="Label"]').each(($input) => {
-            cy.wrap($input)
-                .invoke('val')
-                .then((value) => {
-                    expect(value).to.not.equal('Help');
-                })
-        })
-    }
+  get navigationLabels() {
+    return cy.get("#settings-navigation input[placeholder='Label']");
+  }
+
+  editLastLabel(newValue) {
+    this.navigationLabels.then((inputs) => {
+      cy.wait(500);
+      cy.wrap(inputs[inputs.length - 2])
+        .invoke("val", "")
+        .type(newValue);
+    });
+  }
+
+  get clickHelpDeleteButton() {
+    return cy
+      .get("button")
+      .filter(':contains("Delete")')
+      .last()
+      .then(($button) => {
+        cy.wrap($button).click();
+      });
+  }
+  get validateDeletedLink() {
+    return cy.get('input[placeholder="Label"]').each(($input) => {
+      cy.wrap($input)
+        .invoke("val")
+        .then((value) => {
+          expect(value).to.not.equal("Help");
+        });
+    });
+  }
 }
