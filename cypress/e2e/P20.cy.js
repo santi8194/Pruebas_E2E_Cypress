6573243576869c3abbb39631449f6184fc456346
Camilo Ramírez R <c.ramirezr2@uniandes.edu.co>
2023-05-06 22:44:10 -0500

    sección GIVEN de P20.cy.js

--- cypress/e2e/P20.cy.js
+++ cypress/e2e/P20.cy.js
@@ -1,8 +1,19 @@
+import AdminMenu from "../support/elements/adminMenu";
 
-
+const adminMenu = new AdminMenu();
 
 describe('Eliminar link de navegación', () => {
     it('Elimina el link con el Label Help', () => {
-
+        /*
+ -------------
+ GIVEN
+ -------------
+ */
+        // Autenticar usuario
+        cy.login();
+        cy.wait(10000);
+        // Ir a la pestaña Design
+        adminMenu.designTab.click();
+        cy.wait(1000);
     })
 })
