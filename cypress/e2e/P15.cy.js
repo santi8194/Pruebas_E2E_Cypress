5ea56b409d1b03865d51e9e5dee39cc88cbcb954
julianpuentesuribe <w.puentes@uniandes.edu.co>
2023-05-10 23:36:14 -0500

    Merge screenshots escenario 15

--- cypress/e2e/P15.cy.js
+++ cypress/e2e/P15.cy.js
@@ -16,20 +16,27 @@ describe("Suspender usuario", () => {
 
     // Autentica un usuario que puede cambiar contraseñas
     cy.login();
+    cy.wait(1000);
+    cy.screenshot("15.1 Login v3.41")
 
     // Va a la pestaña Tags
     adminMenu.staffTab.click();
     cy.wait(1000);
+    cy.screenshot("15.2 Click Staff v3.41")
 
     // Ingresa al Usuario que la va a cambiar la contraseña
     staffSection.staffInList().click();
-    cy.wait(2000);
+    cy.wait(1000);
+    cy.screenshot("15.3 Buscar usuario Ghost v3.41")
 
     //Cambiar contraseña para el usuario Ghosh
     const password = Cypress.env("password");
     staffSection.replacePass(password);
+    cy.wait(1000);
+    cy.screenshot("15.4 Reemplazar Pass usuario Ghost v3.41")
     staffSection.changePass.click();
-    cy.wait(2000);
+    cy.wait(1000);
+    cy.screenshot("15.5 Almacenar Pass usuario Ghost v3.41")
 
     /* 
     -------------
@@ -39,20 +46,24 @@ describe("Suspender usuario", () => {
 
     // Clic en lista desplegable de ajustes
     staffSection.staffProfileConfiguration.click({ force: true });
+    cy.wait(1000);
+    cy.screenshot("15.6 Selección settings v3.41")
     // Suspender usuario
     staffSection.clickInSuspendStaffMember.click();
     cy.wait(1000);
+    cy.screenshot("15.7 Selección Suspender v3.41")
     staffSection.suspendStaffMember.click();
     cy.wait(1000);
+    cy.screenshot("15.8 Suspender Usuario v3.41")
     // Volver a la pestaña Staff
     adminMenu.staffTab.click();
     cy.wait(1000);
-
-    //Salir de la sesión actual
-    adminMenu.staffTab.click();
-    cy.wait(2000);
+    cy.screenshot("15.9 Click staff v3.41")
+    
     const baseUrl = Cypress.config("baseUrl");
     cy.visit(baseUrl + "#/signout");
+    cy.wait(1000);
+    cy.screenshot("15.10 Singout v3.41")
     /* 
     -------------
       THEN
@@ -62,5 +73,6 @@ describe("Suspender usuario", () => {
     const userghost = Cypress.env("userghost");
     cy.loginWithCredentials(userghost, password);
     cy.wait(1000);
+    cy.screenshot("15.11 Ingresar usuario ghost v3.41")
   });
 });
