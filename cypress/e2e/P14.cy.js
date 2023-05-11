c88819571134759c48bb03ff5dfeb070da4942ca
julianpuentesuribe <w.puentes@uniandes.edu.co>
2023-05-10 23:26:58 -0500

    Merge screenshots escenario 14

--- cypress/e2e/P14.cy.js
+++ cypress/e2e/P14.cy.js
@@ -16,14 +16,18 @@ describe("Cambiar la contrasenia de un usuario.", () => {
 
     // Autentica un usuario que puede cambiar contraseñas
     cy.login();
+    cy.wait(1000);
+    cy.screenshot("14.1 Login v3.41")
 
     // Va a la pestaña Tags
     adminMenu.staffTab.click();
     cy.wait(1000);
+    cy.screenshot("14.2 click staff v3.41")
 
     // Ingresa al Usuario que la va a cambiar la contraseña
     staffSection.staffInList().click();
-    cy.wait(2000);
+    cy.wait(1000);
+    cy.screenshot("14.3 click usuario Ghost v3.41")
 
     /* 
     -------------
@@ -33,16 +37,20 @@ describe("Cambiar la contrasenia de un usuario.", () => {
     //Cambiar contraseña para el usuario Ghosh
     const password = Cypress.env("password");
     staffSection.replacePass(password);
+    cy.wait(1000);
+    cy.screenshot("14.4 Cambio contraseña v3.41")
     staffSection.changePass.click();
-    cy.wait(2000);
+    cy.wait(1000);
+    cy.screenshot("14.5 Guarda contraseña v3.41")    
     
     //Salir de la sesión actual    
     adminMenu.staffTab.click();
-    cy.wait(2000);
+    cy.wait(1000);
+    cy.screenshot("14.6 click staff v3.41")    
     const baseUrl = Cypress.config("baseUrl");    
     cy.visit(baseUrl + '#/signout');
-    //cy.visit('http://localhost:2368/ghost/#/signout');    
-
+    cy.wait(1000);
+    cy.screenshot("14.7 Cierre de sesión v3.41")
     /* 
     -------------
       THEN
@@ -51,6 +59,7 @@ describe("Cambiar la contrasenia de un usuario.", () => {
     // Verifica que el cambio de contraseña ha sido correcto
     const userghost = Cypress.env("userghost");
     cy.loginWithCredentials(userghost, password);
-    cy.wait(1000);    
+    cy.wait(1000);
+    cy.screenshot("14.8 Autenticación usuario Ghost v3.41")    
   });
 });
