28fe111908af3e1c9066ed2a5fdc8b5d797691a3
julianpuentesuribe <w.puentes@uniandes.edu.co>
2023-05-10 23:09:21 -0500

    Merge screenshots escenario 12

--- cypress/e2e/P11.cy.js
+++ cypress/e2e/P11.cy.js
@@ -19,12 +19,12 @@ describe("Creación de un tag y validación de la creación del tag.", () => {
     // Autentica un usuario que puede crear tags    
     cy.login();
     cy.wait(1000);
-    cy.screenshot("16.1 Login v3.41")
+    cy.screenshot("11.1 Login v3.41")
 
     // Va a la pestaña Tags
     adminMenu.tagTab.click();
     cy.wait(1000);
-    cy.screenshot("16.2 Click Tag v3.41")
+    cy.screenshot("11.2 Click Tag v3.41")
     /* 
     -------------
       WHEN
@@ -38,12 +38,12 @@ describe("Creación de un tag y validación de la creación del tag.", () => {
 
     tagSection.createTag(title, slug , content);
     cy.wait(1000);
-    cy.screenshot("16.3 Agregar info tag v3.41")
+    cy.screenshot("11.3 Agregar info tag v3.41")
 
     // Publica el tag
     tagSection.saveTag.click();
     cy.wait(1000);
-    cy.screenshot("16.4 Guarda tag v3.41")
+    cy.screenshot("11.4 Guarda tag v3.41")
     /* 
     -------------
       THEN
@@ -53,9 +53,9 @@ describe("Creación de un tag y validación de la creación del tag.", () => {
     // Verifica que el tag aparezca en el listado de tags
     adminMenu.tagTab.click();
     cy.wait(1000);
-    cy.screenshot("16.5 Click Tag v3.41")
+    cy.screenshot("11.5 Click Tag v3.41")
     tagSection.tagInList(title).click();
-    cy.screenshot("16.6 Busca Tag v3.41")
+    cy.screenshot("11.6 Busca Tag v3.41")
     cy.wait(2000);
   });
 });
