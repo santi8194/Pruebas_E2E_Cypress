ceaaeb24f33c5be0c30abe13961d3ebf855231b8
Camilo Ramirez R <c.ramirezr2@uniandes.edu.co>
2023-05-10 23:47:03 -0500

    Screenshots in P18.cy.js

--- cypress/e2e/P18.cy.js
+++ cypress/e2e/P18.cy.js
@@ -14,9 +14,13 @@ GIVEN
 */
         // Autenticar usuario
         cy.login();
+        cy.screenshot('P18.1 Login v3.41.1');
         // Design Ir a la pestaña Design
         adminMenu.designTab.click();
         cy.wait(1000);
+        cy.then(() => {
+            cy.screenshot('P18.2 Click_Design v3.41.1');
+        });
         /*
 -------------
 WHEN
@@ -25,9 +29,15 @@ WHEN
         // Agregar Label AnotherHome con url http://localhost:2368/AnotherHome/
         designSection.createLink(label);
         cy.wait(1000);
+        cy.then(() => {
+            cy.screenshot('P18.3 Create_Link v3.41.1');
+        });
         // Guardar cambios
         designSection.saveButton.click();
-        cy.wait(2000);
+        cy.wait(1000);
+        cy.then(() => {
+            cy.screenshot('P18.4 Save_Changes v3.41.1');
+        });
         /*
 -------------
 THEN
@@ -35,5 +45,6 @@ THEN
 */
         // Verificar existencia de nuevo url
         designSection.verifyNewLabel;
+        cy.screenshot('P18.5 Verify_New_Label v3.41.1');
     })
 })
