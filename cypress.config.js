986f02dbe1205e6fd056177dfb9025981bc8f446
Laura Molina <ld.molina11@uniandes.edu.co>
2023-05-11 22:52:33 -0500

    Changed config for 10 scenarios

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,8 @@ module.exports = defineConfig({
       userghost: "ghost-author@example.com",
       versionGhost: "4.44.0",
     },
+    specPattern: "cypress/e2e_10_scenarios/*.cy.js",
+    screenshotsFolder: "cypress/screenshots_10_scenarios",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
